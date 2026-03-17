5f2310c67062ebc6896f0ad70f5d435b12753fde
VeDaNsH-D <vedanshdubeyd6@gmail.com>
2026-03-17 07:43:34 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const scanRoutes = require("./routes/scan");
 const threatRoutes = require("./routes/threatRoutes");
 
 const app = express();
-const PORT = Number(process.env.PORT) || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json({ limit: "12mb" }));
@@ -79,7 +79,7 @@ mongoose
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => {
-      console.log(`Scamurai backend running on http://localhost:${PORT}`);
+      console.log(`Scamurai backend running on port ${PORT}`);
     });
   })
   .catch((error) => {
